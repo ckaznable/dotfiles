acf4de8ca5c4651d5404f4dce65096456c9ab44e
ckaznable <ckaznable@gmail.com>
2023-12-19 10:38:27 +0800

    chore(nvim): update colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,53 @@
 return {
   -- add gruvbox
   { "rebelot/kanagawa.nvim" },
-  { "EdenEast/nightfox.nvim" },
   { "olimorris/onedarkpro.nvim" },
-
-  -- Configure LazyVim to load gruvbox
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+          integrations = {
+              cmp = true,
+              dashboard = true,
+              flash = true,
+              mason = true,
+              gitsigns = true,
+              nvimtree = true,
+              treesitter = true,
+              notify = false,
+              which_key = false,
+              mini = {
+                  enabled = true,
+                  indentscope_color = "frappe",
+              },
+              native_lsp = {
+                  enabled = true,
+                  virtual_text = {
+                      errors = { "italic" },
+                      hints = { "italic" },
+                      warnings = { "italic" },
+                      information = { "italic" },
+                  },
+                  underlines = {
+                      errors = { "underline" },
+                      hints = { "underline" },
+                      warnings = { "underline" },
+                      information = { "underline" },
+                  },
+                  inlay_hints = {
+                      background = true,
+                  },
+              },
+          }
+      })
+    end
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa-wave",
+      colorscheme = "catppuccin-frappe",
     },
   },
 }
