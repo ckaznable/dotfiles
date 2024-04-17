fb6fdbb8be2ee70d1c5a426ede9236cf1f704628
ckaznable <ckaznable@gmail.com>
2024-04-17 14:56:22 +0800

    chore: update nvim plugin

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,6 @@
 
 vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("i", "kk", "<Esc>")
+
+vim.keymap.set("n", "<leader>fd", "<cmd>Telescope dir live_grep<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>fa", "<cmd>Telescope dir find_files<CR>", { noremap = true, silent = true })
