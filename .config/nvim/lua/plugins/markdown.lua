6c70409bd04b1cba8e54c829a64d0eac8a7d26bf
ckaznable <ckaznable@gmail.com>
2024-05-17 09:37:38 +0800

    chore: add Preview plugin for nvim

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,8 +1,9 @@
 return {
   {
-      "iamcco/markdown-preview.nvim",
-      cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-      ft = { "markdown" },
-      build = function() vim.fn["mkdp#util#install"]() end,
-  }
+    "henriklovhaug/Preview.nvim",
+    cmd = { "Preview" },
+    config = function()
+      require("preview").setup()
+    end,
+  },
 }
