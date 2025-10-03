57fb9de1e4a3567594d12e43110aa0e94128e331
ckaznable <ckaznable@gmail.com>
2025-10-03 13:37:35 +0800

    chore: bump nvim

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -3,15 +3,6 @@ return {
   {"nvim-tree/nvim-web-devicons"},
   {"ap/vim-css-color"},
   {
-		"NachoNievaG/atac.nvim",
-		dependencies = { "akinsho/toggleterm.nvim" },
-		config = function()
-			require("atac").setup({
-				dir = "~/.config/atac/", -- By default, the dir will be set as /tmp/atac
-			})
-		end,
-	},
-  {
       'tzachar/local-highlight.nvim',
       config = function()
         require('local-highlight').setup()
